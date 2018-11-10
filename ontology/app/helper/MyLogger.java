79f74aa3442639e951f46f9b01651d7af7865b4d
ylu36 <ylu36@ncsu.edu>
2018-11-10 17:35:25 -0500

    add acceptance logging

--- ontology/app/helper/MyLogger.java
+++ ontology/app/helper/MyLogger.java
@@ -1,14 +1,27 @@
 package helper;
 import java.io.*;
-// import org.drools.spi.KnowledgeHelper;
 
 public class MyLogger {
-    public static void log(String text) {
+    public static void success_log(String text) {
         BufferedWriter output = null;
         try {
-            File file = new File("aa.log");
+            File file = new File("success.log");
             output = new BufferedWriter(new FileWriter(file, true));
-            output.write(text );//+ " with " + drools.getRule().getName());
+            output.write(text);
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+          if ( output != null ) {
+                try {output.close();} catch (Exception ex) {/*ignore*/}
+          }
+        }
+    }
+    public static void fail_log(String text) {
+        BufferedWriter output = null;
+        try {
+            File file = new File("failure.log");
+            output = new BufferedWriter(new FileWriter(file, true));
+            output.write(text);
         } catch ( IOException e ) {
             e.printStackTrace();
         } finally {
